b7c057c8125870709f80d05f39aa68314998b04f
zbnorwey <zb.norwey@gmail.com>
2026-02-27 06:49:04 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,16 @@
-const CACHE_NAME = 'jubiler-v1';
-const ASSETS = [
-  'index.html',
-  'manifest.json',
-  'https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js'
+const CACHE_NAME = 'ROSSO';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json',
+  './assets/icons/rosso-192.png',
+  './assets/icons/rosso-279.png'
 ];
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
 });
 
-self.addEventListener('fetch', (e) => {
+self.addEventListener('fetch', e => {
   e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
